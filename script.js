f23af6624956d9739ef5b0f06fda21d36b429926
Michiel <michiel.huijse@gmail.com>
2016-08-23 21:04:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
- src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
- src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
- 
+
  $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
